4565bc15acd93bd99a633a1a0b3b2a61e7da2a51
CJJ <455984146@qq.com>
2020-09-06 22:32:47 +0800

    最长上升子序列 leetcode--300

---
+++ Week_09/LengthOfLIS.java
@@ -0,0 +1,41 @@
+/**
+ * @Deseription 最长上升子序列
+ * leetcode--300
+ **/
+public class LengthOfLIS {
+
+    /**
+     * use dp
+     * @param nums
+     * @return
+     */
+    public static int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < dp.length; i++) {
+            int maxval = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    maxval = Math.max(maxval, dp[j]);
+                }
+            }
+            dp[i] = maxval + 1;
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+
+    /**
+     * run test
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] nums = new int[]{10,9,2,5,3,7,101,18};
+        int length = lengthOfLIS(nums);
+        System.out.println("result:"+length);
+    }
+}
